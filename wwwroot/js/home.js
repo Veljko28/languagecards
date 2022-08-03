96eac52b68fdfa8551dd48ad8be00d0afca631f7
Veljko28 <charlibear28@gmail.com>
2022-08-03 22:28:04 +0200

    home page js and css

---
+++ wwwroot/js/home.js
@@ -0,0 +1,10 @@
+﻿const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+
+
+open.addEventListener('click', () => {
+    modal_container.classList.add('show');
+    open.classList.add('hide');
+    document.getElementById('welcome').classList.add('hide');
+    document.getElementById('footer').classList.add('dark');
+})
