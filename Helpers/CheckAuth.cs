f8ceccbcb2a88d3a7f621f758e6af9c214b743f4
Veljko28 <charlibear28@gmail.com>
2022-08-05 22:26:45 +0200

    adding check auth helper

---
+++ Helpers/CheckAuth.cs
@@ -0,0 +1,21 @@
+﻿using LanguageCards.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageCards.Helpers
+{
+	public static class CheckAuth
+	{
+		public static bool Authenticate(KeyValuePair<string, string> cookie)
+		{
+			if (cookie.Value != null && cookie.Value == AdminModel.Password)
+			{
+				return true;
+			}
+			else return false;
+		}
+	}
+}
