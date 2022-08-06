86952b71e58b99debfc4d9ab744f08e5998a7262
Veljko28 <charlibear28@gmail.com>
2022-08-06 21:13:07 +0200

    adding spanish controller

---
+++ Controllers/SpanishController.cs
@@ -0,0 +1,23 @@
+﻿using LanguageCards.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageCards.Controllers
+{
+	public class SpanishController : Controller
+	{
+			[HttpGet("/spanish")]
+			public IActionResult Index()
+			{
+				var cookie = (Request.Cookies.Where(x => x.Key == "LoggedIn")).FirstOrDefault();
+				if (CheckAuth.Authenticate(cookie))
+				{
+					return View();
+				}
+				else return RedirectToAction("Index", "Home");
+			}
+	}
+}
