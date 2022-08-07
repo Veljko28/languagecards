bd6a3782f1084f5b45416a0f25e4a731bdf1baca
Veljko28 <charlibear28@gmail.com>
2022-08-07 16:47:06 +0200

    adding error message view model for login

---
+++ Models/ErrorMessageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageCards.Models
+{
+	public class ErrorMessageViewModel
+	{
+
+		public ErrorMessageViewModel(string msg)
+		{
+			Message = msg;
+		}
+		public string Message { get; set; } = "";
+	}
+}
