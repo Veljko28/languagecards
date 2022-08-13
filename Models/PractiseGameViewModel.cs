d7541589d6c6bc442a529fe766ac8017d5517353
Veljko28 <charlibear28@gmail.com>
2022-08-13 15:41:35 +0200

    adding practise game view model

---
+++ Models/PractiseGameViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LanguageCards.Models
+{
+    public class PractiseGameViewModel
+    {
+        public int HearthsLeft { get; set; } = 3;
+        public int CorrectAnswers { get; set; } = 0;
+        public string TimePassed { get; set; } = "00:00";
+        public string WordToFind { get; set; } = "";
+        public int AnswerType { get; set; } = 0; // if 0 no display, if 1 display that the answer is correct, if 2 incorrect
+    }
+}
