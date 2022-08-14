3c089e0839b6d72d291ff9d2336412cc9d37b267
Veljko28 <charlibear28@gmail.com>
2022-08-14 15:36:33 +0200

    new javascript and css

--- wwwroot/js/timer.js
+++ wwwroot/js/timer.js
@@ -2,6 +2,12 @@
     const timer = document.getElementById("timer");
     let a = timer.innerHTML;
     let m = parseInt(a.substring(0, 2)), s = parseInt(a.substring(3, 5));
+    const hearths = document.getElementById("hearths");
+    if (hearths.innerHTML == "0") {
+        alert("You lost all your hearths!");
+        location.href = "/russian";
+        clearInterval(startInterval);
+    }
     if (s == 59) {
         if (m == 14) {
             alert("This lesson took too long. It will be finished !");
@@ -18,3 +24,19 @@
 
 
 var startInterval = setInterval(() => getTimePassed(), 1000);
+const sbm = document.getElementById("sbm");
+
+sbm.addEventListener("click", (e) => {
+    e.preventDefault();
+    const alr = document.getElementById("alert-type");
+    const suc = document.getElementById("sucs"), fld = document.getElementById("fld");
+    switch (alr.value) {
+        case 0:
+            break;
+        case 1:
+            suc.classList.add("show");
+        case 2:
+            fld.classList.add("show");
+        default: break;
+    }
+})
