108b4da892ddf1b2338c4141f07b060bd0994841
Veljko28 <charlibear28@gmail.com>
2022-08-15 17:39:15 +0200

    adding answer type to practise model

--- Models/PractiseGameViewModel.cs
+++ Models/PractiseGameViewModel.cs
@@ -7,5 +7,6 @@
         public string TimePassed { get; set; } = "00:00";
         public string WordToFind { get; set; } = "";
         public string AnswerType { get; set; } = "0"; // if 0 no display, if 1 display that the answer is correct, if 2 incorrect
+        public int QuestionType { get; set; } = 1;
     }
 }
