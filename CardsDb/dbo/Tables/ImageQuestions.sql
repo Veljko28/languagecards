0530fdce81749ad0dcb42499e4fc813779efefbd
Veljko28 <charlibear28@gmail.com>
2022-08-16 22:38:20 +0200

    adding image question table

---
+++ CardsDb/dbo/Tables/ImageQuestions.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ImageQuestions]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[ImagePath] NVARCHAR(100) NOT NULL,
+	[Translation] NVARCHAR(100) NOT NULL,
+	[Language] NVARCHAR(50) NOT NULL,
+)
