7484718cee15b7bd288d3b525f6b74d08d7397fa
Veljko28 <charlibear28@gmail.com>
2022-08-16 22:38:31 +0200

    adding question table

---
+++ CardsDb/dbo/Tables/Questions.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Questions]
+(
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[Word] NVARCHAR(100) NOT NULL,
+	[Translation] NVARCHAR(100) NOT NULL,
+	[Language] NVARCHAR(50) NOT NULL,
+	[QuestionType] bit NOT NULL
+)
