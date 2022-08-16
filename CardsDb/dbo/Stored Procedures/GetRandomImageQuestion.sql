36ab560e3a97a2811e93569c40b6c36900269eb2
Veljko28 <charlibear28@gmail.com>
2022-08-16 22:38:57 +0200

    adding stored procedure to get random image questions

---
+++ CardsDb/dbo/Stored Procedures/GetRandomImageQuestion.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[GetRandomImageQuestion]
+	@LanguageType NVARCHAR(50)
+AS
+begin
+	SELECT TOP 1 * FROM [dbo].[ImageQuestions] WHERE [Language] = @LanguageType ORDER BY NEWID();
+end
