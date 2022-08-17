b92c087beba4e0e19a3b620d991ddcf7ef193d37
Veljko28 <charlibear28@gmail.com>
2022-08-17 21:38:24 +0200

    adding the question model for the db

---
+++ LanguageCards/Models/DbModels/QuestionModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LanguageCards.Models.DbModels
+{
+    public class QuestionModel
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string Translation { get; set; }
+        public string Language { get; set; }
+        public bool QuestionType { get; set; }
+
+    }
+}
