ce5041811a058fd9a20427497982fa9a92c6d0a8
Veljko28 <charlibear28@gmail.com>
2022-08-17 21:38:15 +0200

    adding the image question model for the db

---
+++ LanguageCards/Models/DbModels/ImageQuestionModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LanguageCards.Models.DbModels
+{
+    public class ImageQuestionModel
+    {
+        public int Id { get; set; }
+        public string ImagePath { get; set; }
+        public string Translation { get; set; }
+        public string Language { get; set; }
+    }
+}
