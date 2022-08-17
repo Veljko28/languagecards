c6ce79e45e0d9424c157c4467a05bf417f31aab9
Veljko28 <charlibear28@gmail.com>
2022-08-17 21:38:03 +0200

    adding the db context

---
+++ LanguageCards/Helpers/CardDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LanguageCards.Models.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanguageCards.Helpers
+{
+    public class CardDbContext : DbContext
+    {
+        public CardDbContext() : base()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=CardsDb;Trusted_Connection=True");
+        }
+
+        public DbSet<QuestionModel> Questions { get; set; }
+        public DbSet<ImageQuestionModel> ImageQuestions { get; set; }
+    }
+}
