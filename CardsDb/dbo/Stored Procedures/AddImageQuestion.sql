8a2fed2b4b976b3aa0219a987fba3bcc10e8793d
Veljko28 <charlibear28@gmail.com>
2022-08-25 21:15:50 +0200

    adding image question stored procedure

---
+++ CardsDb/dbo/Stored Procedures/AddImageQuestion.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[AddImageQuestion]
+	@ImagePath NVARCHAR(100),
+	@Translation NVARCHAR(100),
+	@Language NVARCHAR(50)
+AS
+begin
+	INSERT INTO ImageQuestions VALUES (N''+@ImagePath, N''+@Translation, @Language);
+end
