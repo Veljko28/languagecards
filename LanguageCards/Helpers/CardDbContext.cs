93c0690681bebc652634e6e6ee5e82e748194684
Veljko28 <charlibear28@gmail.com>
2022-08-25 21:14:33 +0200

    context for database

--- LanguageCards/Helpers/CardDbContext.cs
+++ LanguageCards/Helpers/CardDbContext.cs
@@ -5,15 +5,15 @@ namespace LanguageCards.Helpers
 {
     public class CardDbContext : DbContext
     {
-        public CardDbContext() : base()
+        public CardDbContext(DbContextOptions options) : base(options)
         {
 
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(
-                @"Server=(localdb)\mssqllocaldb;Database=CardsDb;Trusted_Connection=True");
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(
+        //        @"Server=(localdb)\mssqllocaldb;Database=CardsDb;Trusted_Connection=True");
+        //}
 
         public DbSet<QuestionModel> Questions { get; set; }
         public DbSet<ImageQuestionModel> ImageQuestions { get; set; }
