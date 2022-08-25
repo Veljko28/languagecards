c219dc05a8e49b0198b82ac56e837a0f551ea551
Veljko28 <charlibear28@gmail.com>
2022-08-25 21:15:40 +0200

    adding question store procedure

---
+++ CardsDb/dbo/Stored Procedures/AddQuestion.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[AddQuestion]
+	@Word NVARCHAR(100),
+	@Translation NVARCHAR(100),
+	@Language NVARCHAR(50),
+	@QuestionType BIT
+AS
+begin
+	INSERT INTO Questions VALUES (N''+@Word, N''+@Translation, @Language, @QuestionType);
+end
