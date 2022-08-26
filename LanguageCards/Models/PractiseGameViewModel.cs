19e3c086a891f52f050984eb094d85ae99a162ec
Veljko28 <charlibear28@gmail.com>
2022-08-26 22:39:46 +0200

    practise view model improvements for db

--- LanguageCards/Models/PractiseGameViewModel.cs
+++ LanguageCards/Models/PractiseGameViewModel.cs
@@ -5,8 +5,11 @@
         public int HearthsLeft { get; set; } = 3;
         public int CorrectAnswers { get; set; } = 0;
         public string TimePassed { get; set; } = "00:00";
-        public string WordToFind { get; set; } = "";
+        public string Answer { get; set; } = ""; // Answer given by user
         public string AnswerType { get; set; } = "0"; // if 0 no display, if 1 display that the answer is correct, if 2 incorrect
         public int QuestionType { get; set; } = 1;
+        public string Word { get; set; }
+        public string Translation { get; set; } // database translation / correct answer
+        public string ImagePath { get; set; }
     }
 }
