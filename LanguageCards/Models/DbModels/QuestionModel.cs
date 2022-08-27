7ca64b038325ba50b8a5f442b28cfe9d93b29aa5
Veljko28 <charlibear28@gmail.com>
2022-08-27 14:32:51 +0200

    question model removing question type

--- LanguageCards/Models/DbModels/QuestionModel.cs
+++ LanguageCards/Models/DbModels/QuestionModel.cs
@@ -6,7 +6,6 @@
         public string Word { get; set; }
         public string Translation { get; set; }
         public string Language { get; set; }
-        public bool QuestionType { get; set; }
 
     }
 }
