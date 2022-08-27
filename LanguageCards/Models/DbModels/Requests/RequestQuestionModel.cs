1894e2a9d9be7eaa8944b2341e70cb7a68e81776
Veljko28 <charlibear28@gmail.com>
2022-08-27 14:32:40 +0200

    request question from db model

---
+++ LanguageCards/Models/DbModels/Requests/RequestQuestionModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LanguageCards.Models.DbModels.Requests
+{
+    public class RequestQuestionModel
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string Language { get; set; }
+    }
+}
