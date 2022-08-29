ec2034ec9194ca95a938b932268f63d5614b631e
Veljko28 <charlibear28@gmail.com>
2022-08-29 17:41:03 +0200

    editing a word with a stored procedure

---
+++ CardsDb/dbo/Stored Procedures/EditWord.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[EditWord]
+	@EditId int,
+	@EditWord NVARCHAR(100),
+	@EditTranslation NVARCHAR(100)
+AS
+begin
+	UPDATE [dbo].[Questions] SET Word = @EditWord, Translation = @EditTranslation WHERE Id = @EditId;
+end
